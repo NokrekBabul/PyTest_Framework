f110f40e93dbddd0496110232f1e591756f4222f
NokrekBabul <codingandtestingdaily@gmail.com>
2019-02-06 00:03:35 -0600

    Initial commit on PyTest

---
+++ PytestFramework/test_Framework.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+import pytest
+
+
+class Test_SignIn():
+    @pytest.fixture()
+    def test_setUp(self):
+        global driver
+        driver = webdriver.Chrome(executable_path="C:/Users/theda/PycharmProjects/Framework/drivers/chromedriver.exe")
+        driver.implicitly_wait(5)
+        driver.maximize_window()
+
+        yield
+        driver.close()
+        driver.quit()
+        print("Test Completed")
+
+
+    def test_GoFundMeLogin_1(self, test_setUp):
+        driver.get("https://gofundme.com")
+        driver.find_element_by_link_text("Sign in").click()
+        driver.find_element_by_id("email").send_keys("codingandtestingdaily@gmail.com")
+        driver.find_element_by_name("password").send_keys("abcd1234")
+        driver.find_element_by_tag_name("button").click()
+
+        x = driver.title
+        print(x)
+        assert x == "Sign In | GoFundMe"
+
+    def test_GoFundMeLogin_2(self, test_setUp):
+        driver.get("https://gofundme.com")
+        driver.find_element_by_link_text("Sign in").click()
+        driver.find_element_by_id("email").send_keys("codingandtestingdaily@gmail.com")
+        driver.find_element_by_name("password").send_keys("abcd1234")
+        driver.find_element_by_tag_name("button").click()
+
+        x = driver.title
+        print(x)
+        assert x == "Sign in | GoFundMe"
+
+
+    # def test_tearDown():
+    #     driver.close()
+    #     driver.quit()
+    #     print("Test Completed")
+
